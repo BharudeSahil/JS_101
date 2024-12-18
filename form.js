4e41e5efd39304b8563fdf50006139c7d79d1ebb
BharudeSahil <bharudesahil@gmail.com>
2024-12-18 13:34:16 +0530

    form error changed

--- form.js
+++ form.js
@@ -3,5 +3,13 @@ function input(){
     let email = document.getElementById("email").value
     let div = document.getElementById("div").value
 
-    document.getElementById("myp").textContent = `Thank you ${name} for your Feedback `
+    console.log("Name:", name);
+    console.log("Email:", email);
+    console.log("Div:", div);
+
+
+    document.getElementById("myp").textContent = `Thank you ${name} for your Feedback`;
+    
 }
+
+// document.getElementById("myp").textContent = `Thank you ${name} for your Feedback `
